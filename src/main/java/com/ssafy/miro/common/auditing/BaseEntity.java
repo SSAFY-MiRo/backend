d7ee4f8b3655fd0ab1f2e3e12cd96b5fde1992d3
ryulkim <hobby519@naver.com>
2024-11-17 19:20:00 +0900

    feat: cors 기능 추가

---
+++ src/main/java/com/ssafy/miro/common/auditing/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.ssafy.miro.common.auditing;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity{
+}
