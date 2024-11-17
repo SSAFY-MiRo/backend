0134ae682e0d2ef5b8970c0fdf8bd1f826ec4dd1
ryulkim <hobby519@naver.com>
2024-11-17 16:57:43 +0900

    feat: 공통 응답 코드 기능 추가

---
+++ src/main/java/com/ssafy/miro/common/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.ssafy.miro.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.ssafy.miro.common.code.ErrorCode;
+import com.ssafy.miro.common.code.SuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public record ApiResponse<T>(@JsonProperty("isSuccess") boolean isSuccess,
+                             int code,
+                             String message,
+                             @JsonInclude(JsonInclude.Include.NON_NULL) T result) {
+    // 성공한 경우
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, SuccessCode.OK.getCode(), SuccessCode.OK.getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> of(T result, SuccessCode successCode) {
+        return new ApiResponse<>(true, successCode.getCode(), successCode.getMessage(), result);
+    }
+
+    // 실패한 경우
+    public static <T> ApiResponse<T> onFailure(ErrorCode errorCode, T result) {
+        return new ApiResponse<>(false, errorCode.getCode(), errorCode.getMessage(), result);
+    }
+
+}
---
+++ src/main/java/com/ssafy/miro/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.ssafy.miro.common.exception;
+
+import com.ssafy.miro.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.ssafy.miro.common.code.ErrorCode.*;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException e,
+            final HttpHeaders headers,
+            final HttpStatusCode status,
+            final WebRequest request
+    ) {
+        log.warn(e.getMessage(), e);
+        String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.onFailure(INVALID_REQUEST, null));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleException(final Exception e){
+        log.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError()
+                .body(ApiResponse.onFailure(SERVER_ERROR, null));
+    }
+}
