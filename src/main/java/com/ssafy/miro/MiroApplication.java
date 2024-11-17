2d86a17b3d8190a4794b84de139e9875230a3a44
sehako <dhtpgkr1999@gmail.com>
2024-11-17 15:33:47 +0900

    build: 프로젝트 생성 완료

---
+++ src/main/java/com/ssafy/miro/MiroApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy.miro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiroApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiroApplication.class, args);
+    }
+}
