cd60b3044e393e72cd2426c68ffc9a53b40853d1
김률아 <hobby519@naver.com>
2024-11-21 10:35:34 +0900

    feat: user

--- src/main/java/com/ssafy/miro/common/code/ErrorCode.java
+++ src/main/java/com/ssafy/miro/common/code/ErrorCode.java
@@ -23,10 +23,13 @@ public enum ErrorCode {
     EXPIRED_REFRESH_TOKEN(2007, UNAUTHORIZED, "만료된 RefreshToken 입니다."),
     FAIL_OAUTH_USERINFO_RETRIEVAL(2008, UNAUTHORIZED, "회원 정보를 가져오는데 실패했습니다."),
 
+    //User
     NOT_FOUND_BOARD_ID(3001, NOT_FOUND, "요청한 ID에 해당하는 게시물이 존재하지 않습니다."),
     NOT_FOUND_USER_ID(3002, NOT_FOUND, "요청한 ID에 해당하는 사용자가 존재하지 않습니다."),
     NOT_FOUND_USER_EMAIL(3003, NOT_FOUND, "요청한 이메일에 해당하는 사용자가 존재하지 않습니다."),
     NOT_FOUND_IMAGE_FILE(3004, NOT_FOUND, "요청한 이미지 파일을 찾을 수 없습니다."),
+    EMAIL_DUPLICATED(3005, CONFLICT, "이메일이 이미 존재합니다."),
+    NON_VALIDATED_PASSWORD(3006, UNAUTHORIZED, "비밀번호가 유효하지 않습니다."),
 
     //Attraction
     NOT_FOUND_ATTRACTION_ID(3101, NOT_FOUND, "요청한 ID에 해당하는 관광지가 존재하지 않습니다."),
--- src/main/java/com/ssafy/miro/user/application/UserService.java
+++ src/main/java/com/ssafy/miro/user/application/UserService.java
@@ -3,10 +3,17 @@ package com.ssafy.miro.user.application;
 import com.ssafy.miro.user.application.response.UserInfo;
 import com.ssafy.miro.user.domain.User;
 import com.ssafy.miro.user.domain.repository.UserRepository;
+import com.ssafy.miro.user.exception.EmailDuplicateException;
+import com.ssafy.miro.user.exception.NonValidationPasswordException;
+import com.ssafy.miro.user.exception.UserNotFoundException;
 import com.ssafy.miro.user.presentation.request.UserCreateRequest;
 import com.ssafy.miro.user.presentation.request.UserLoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.ssafy.miro.common.code.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +21,38 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void createUser(UserCreateRequest userCreateRequest) {
+        findByEmail(userCreateRequest.email()).ifPresent(user->{throw new EmailDuplicateException(EMAIL_DUPLICATED);});
         userRepository.save(userCreateRequest.toUser());
     }
 
     public UserInfo loginUser(UserLoginRequest userLoginRequest) {
-        User user = userRepository.findByEmail(userLoginRequest.email());
+        User user=findUserByEmail(userLoginRequest.email());
         return UserInfo.of(user);
+    }
+
+    public void validatePassword(String email, String password) {
+        User user=findUserByEmail(email);
+        if(!user.getPassword().equals(password)) {
+            throw new NonValidationPasswordException(NON_VALIDATED_PASSWORD);
+        }
+    }
+
+    public UserInfo getUserInfo(Long id) {
+        User user=findById(id);
+        return UserInfo.of(user);
+    }
+
+    private User findById(Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(NOT_FOUND_USER_ID));
+    }
+
+
+    private Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+    private User findUserByEmail(String email) {
+        return findByEmail(email).orElseThrow(()->new UserNotFoundException(NOT_FOUND_USER_EMAIL));
     }
 }
