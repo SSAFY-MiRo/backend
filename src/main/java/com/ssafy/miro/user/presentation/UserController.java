767ff682d06436b22f48b92f3ab6b651a1dfde5c
김률아 <hobby519@naver.com>
2024-11-24 23:10:41 +0900

    feat: Feature/7 into develop

--- src/main/java/com/ssafy/miro/user/presentation/UserController.java
+++ src/main/java/com/ssafy/miro/user/presentation/UserController.java
@@ -1,11 +1,13 @@
 package com.ssafy.miro.user.presentation;
 
+import com.ssafy.miro.auth.domain.dto.UserToken;
 import com.ssafy.miro.common.ApiResponse;
 import com.ssafy.miro.common.auth.Auth;
 import com.ssafy.miro.image.application.ImageService;
 import com.ssafy.miro.user.application.UserService;
 import com.ssafy.miro.user.application.response.UserInfo;
 import com.ssafy.miro.user.domain.User;
+import com.ssafy.miro.user.domain.UserType;
 import com.ssafy.miro.user.presentation.request.UserCheckPwdRequest;
 import com.ssafy.miro.user.presentation.request.UserCreateRequest;
 import com.ssafy.miro.user.presentation.request.UserLoginRequest;
@@ -31,13 +33,13 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<Object>> createUser(@RequestBody @Valid UserCreateRequest userCreateRequest) {
-        userService.createUser(false, userCreateRequest);
+        userService.createUser(false, userCreateRequest, UserType.INVALID);
         return ResponseEntity.created(URI.create("/user")).body(ApiResponse.of(CREATE_USER, null));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<UserInfo>> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
-        UserInfo userInfo = userService.loginUser(userLoginRequest);
+    public ResponseEntity<ApiResponse<UserToken>> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        UserToken userInfo = userService.loginUser(userLoginRequest);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(userInfo));
     }
 
@@ -55,7 +57,7 @@ public class UserController {
     }
 
     @PutMapping(consumes = "multipart/form-data")
-    public ResponseEntity<ApiResponse<Object>> updateUser(@Auth User user, @RequestParam("file") MultipartFile file, @ModelAttribute @Valid UserUpdateRequest userUpdateRequest) throws IOException {
+    public ResponseEntity<ApiResponse<Object>> updateUser(@Auth User user, @RequestParam(value = "file", required = false) MultipartFile file, @ModelAttribute @Valid UserUpdateRequest userUpdateRequest) throws IOException {
         userService.updateUser(user, file, userUpdateRequest);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(null));
     }
